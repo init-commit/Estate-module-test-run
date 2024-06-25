db4382027e091c0fedc6b4767fcd44f359b9d4ef
M i k e <iammikeian@gmail.com>
2024-06-25 08:36:54 +0300

    :truck: Pick up

--- custom/src/custom_modules/om_estate/models/listings.py
+++ custom/src/custom_modules/om_estate/models/listings.py
@@ -9,12 +9,13 @@ class EstateListings(models.Model):
 
 
     realtor_id = fields.Many2one('estate.agent', string = "Realtor ID")
-
-    # name = fields.Char(string="House Type")
-    # size_in_sq = fields.Float(String="Size in SQ")
-    # status = fields.Selection([('Available', 'Available'), ('Unavailable', 'Unavailable')], string='Status')
-    # purchase_value = fields.Float(string="Purchase Value")
-    # lease_value = fields.Float(string="Lease Value")
-    # phone = fields.Char(string='Phone')
-    # active = fields.Boolean(string="Active", default=True)
+    # listing_date = fields.Date(string = "Listing date", default=fields.Date.context_today)
+    listing_time = fields.Datetime(string = "Listing time", default=fields.Datetime.now)
+    name = fields.Char(string="House Type")
+    size_in_sq = fields.Float(string="Size in SQ")
+    status = fields.Selection([('Available', 'Available'), ('Unavailable', 'Unavailable')], string='Status')
+    purchase_value = fields.Float(string="Purchase Value")
+    lease_value = fields.Float(string="Lease Value")
+    phone = fields.Char(string='Phone')
+    active = fields.Boolean(string="Active", default=True)
 
